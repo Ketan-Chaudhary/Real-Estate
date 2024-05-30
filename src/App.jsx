609a83b9127f0c65b4b56278361ab48a5f88bdce
Ketan Chaudhary <ketanchaudharypoonia@gmail.com>
2024-05-31 01:57:41 +0530

    Partner-images

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
-import Header from "./Components/Header/Header"
+import Header from "./Components/Header/Header";
 import Hero from "./Components/Hero/Hero";
+import Partners from "./Components/Partners/Partners";
 function App() {
   return (
     <div className="App">
       <div>
-        <div className="white-gradient"/>
-      <Header/>
-      <Hero/>
+        <div className="white-gradient" />
+        <Header />
+        <Hero />
       </div>
+      <Partners/>
     </div>
   );
 }
